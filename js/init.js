27b1240b04d13a374498464fd6f9da4556d1019e
tropicalstay <tropical.stay7@gmail.com>
2024-12-11 13:39:33 +0500

    update

---
+++ js/init.js
@@ -0,0 +1,14 @@
+// Edit the date here
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "14 January 2025 09:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
